601b58f823e222c634ccfecd434b80156d81d54d
dengfengdn <128899839+dengfengdn@users.noreply.github.com>
2025-04-09 18:45:28 +0800

    Delete pd_car.py

--- pd_car.py
+++
@@ -1,9 +0,0 @@
-from idlelib.iomenu import encoding
-
-import pandas as pd
-
-file_path='processed_data.csv'
-df=pd.read_csv(file_path,encoding='utf-8')
-#print(df.loc[0:3])
-df.loc[:,'价格范围']=df['价格范围'].str.replace("万","")
-print(df['日期'].isnull)
